f4bfb530f95de5b55f29d653d3855dcb5fa6597b
Matteo Cacciola <matteo.cacciola@gmail.com>
2023-03-27 04:15:54 +0200

    adding tests

---
+++ src/helpers/__tests__/getAnalysisResults.test.ts
@@ -0,0 +1,47 @@
+import { expect, describe, it, vitest, beforeEach, SpyInstance } from 'vitest';
+import { getAnalysisResults } from '../getAnalysisResults';
+import { MediaType, StrategyType } from '../../types';
+import * as strategyProvider from '../../strategies/provider';
+
+let mockedStrategyProvider: SpyInstance;
+
+describe('getAnalysisResults', () => {
+  const company = 'Test Company';
+  const media: MediaType = 'twitter';
+  const items = ['item 1', 'item 2', 'item 3'];
+  const strategyType: StrategyType = 'afinn';
+  const scoreThreshold = 0;
+
+  const evaluateScoresMock = vitest.fn().mockResolvedValue([
+    { score: 0.5, category: 'positive' },
+    { score: -0.5, category: 'negative' },
+    { score: 0, category: 'neutral' },
+  ]);
+
+  beforeEach(() => {
+    mockedStrategyProvider = vitest.spyOn(strategyProvider, 'strategyProvider')
+      .mockReturnValue({ evaluateScores: evaluateScoresMock });  });
+
+  it('should return the expected sentiment stats and scores', async () => {
+    const expectedResult = {
+      sentimentStats: { positive: 1, negative: 1, neutral: 1, undefined: 0 },
+      scores: [
+        { text: 'item 1', score: 0.5, category: 'positive' },
+        { text: 'item 2', score: -0.5, category: 'negative' },
+        { text: 'item 3', score: 0, category: 'neutral' },
+      ],
+    };
+    const result = await getAnalysisResults(company, media, items, strategyType, scoreThreshold);
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('should call strategyProvider with the correct strategy type', async () => {
+    await getAnalysisResults(company, media, items, strategyType, scoreThreshold);
+    expect(mockedStrategyProvider).toHaveBeenCalledWith(strategyType);
+  });
+
+  it('should call evaluateScores with the correct parameters', async () => {
+    await getAnalysisResults(company, media, items, strategyType, scoreThreshold);
+    expect(evaluateScoresMock).toHaveBeenCalledWith(company, items, scoreThreshold);
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     environment: 'node',
     coverage: {
+      exclude: [...configDefaults.exclude, '**/utils/*'],
       provider: 'istanbul',
       reporter: ['text', 'json', 'html'],
       enabled: true,
